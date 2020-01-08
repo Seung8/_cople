5ca7378833d0b8346ed32a2f971bba5841d9d240
Seung8 <lsy0914@naver.com>
2020-01-08 22:57:02 +0900

    자동 감시 주문 조건 수정

--- controllers/transaction.py
+++ controllers/transaction.py
@@ -14,33 +14,33 @@ class TransactionController:
     """
 
     def __init__(self, condition_id):
-        self.condition_id = condition_id
         self.base_url = 'https://api.upbit.com'
+        self.condition_id = condition_id
 
-    # 주문 조건 객체 조회
-    def get_condition(self):
-        condition = None
+        self.__condition = None
 
-        try:
-            model = apps.get_model('transaction', 'OrderCondition')
-            condition = model.objects.get(id=self.condition_id)
-        except Exception as e:
-            # todo: 추후 로깅 대체
-            print(e)
+    @property
+    def condition(self):
+        """주문 조건 객체 반환 """
+        print('set condition')
+        model = apps.get_model('transaction', 'OrderCondition')
+        self.__condition = model.objects.filter(id=self.condition_id).first()
 
-        return condition
+        return self.__condition
 
-    # 코인 시세 조회
-    def get_coin_price(self):
-        condition = self.get_condition()
+    @property
+    def coin_price(self):
+        """주문 조건에 등록된 코인의 시세 조회"""
+        condition = self.condition
 
         if not condition:
             return None
 
         url = self.base_url + '/v1/ticker'
-        params = {'markets': condition.coin.code}
+        params = {'markets': self.condition.coin.code}
         response = requests.get(url, params=params)
 
+        # 가격 조회에 실패하면 해당 예외를 출력(로깅)하고 즉시 while 루프 탈출
         if response.status_code != 200:
             condition.is_active = False
             condition.modified_at = timezone.localtime()
@@ -49,26 +49,31 @@ class TransactionController:
             # todo: 추후 로깅으로 대체
             print(response.text)
 
+            return None
+
         result = json.loads(response.text)
         price = result[0]['trade_price']
 
-        return price
+        return float(price)
 
-    # 매수
     def buy(self):
+        """매수"""
         pass
 
-    # 매도
     def sell(self):
+        """매도"""
         pass
 
-    # 자동 감시 주문 로직 실행
     def run(self):
+        """
+        자동 감시 주문 실행
+
+        만약 while loop 동작 중 조건이 비활성화 된 경우 즉시 while loop 를 탈출해야 하므로
+        매 초마다 주문 조건(self.condition)을 조회
+        """
         while True:
-            time.sleep(1)
-            condition = self.get_condition()
-
-            if not condition or not condition.is_active:
+            condition = self.condition
+            if not condition.is_active:
                 break
-
             print('async function is working!')
+            time.sleep(1)
