b0d80a473671afba95feb45109e2cd11d40659e6
Seung8 <lsy0914@naver.com>
2020-01-13 00:15:54 +0900

    코드 리펙터링 및 pyjwt 추가

--- models/transaction/tasks.py
+++ models/transaction/tasks.py
@@ -3,8 +3,8 @@ from settings import celery_app
 
 
 @celery_app.task
-def observe_condition(condition_id):
-    tc = TransactionController(condition_id)
+def observe_condition(condition_id, user_id):
+    tc = TransactionController(condition_id, user_id)
     tc.run()
 
     return True
